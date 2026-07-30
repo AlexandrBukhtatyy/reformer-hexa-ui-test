ab5c631f0976a9e4f31f0f983494c1308592dd51
Alexandr Bukhtatyy <buhtatyalexander90@gmail.com>
2026-07-30 21:14:43 +0300

    install hexa-ui

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// provide theme color to css vars
+import { GlobalStyle } from '@kaspersky/hexa-ui/design-system/global-style'
+
+// provide base css
+import '@kaspersky/hexa-ui/design-system/global-style/styles.css'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <GlobalStyle />
     <App />
   </StrictMode>,
 )
